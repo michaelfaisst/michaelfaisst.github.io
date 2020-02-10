829f44b043aaaeefdd753ba5ce04aab11aff26e7
Michael Faißt <michael@codenite.at>
2020-02-11 00:31:06 +0100

    removed docs folder

--- docs/styles-3b608394b9f48406c6ad.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{J9yo:function(n,o,w){}}]);
-//# sourceMappingURL=styles-3b608394b9f48406c6ad.js.map
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	pathPrefix: "/save-the-date",
 	siteMetadata: {
 		title: `Michi & Manu - Save the date`,
 		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
